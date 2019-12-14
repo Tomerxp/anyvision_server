bacd4236dbf9ce0f9c5269e9ed3c588c395d3929
Tomer Lilush <tomerxp@gmail.com>
2019-12-14 13:48:12 +0200

    Deployment to Netlify

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 // make bluebird default Promise
 Promise = require('bluebird') // eslint-disable-line no-global-assign
-const { port, env } = require('./config/vars')
-const logger = require('./config/logger')
+const serverless = require('serverless-http')
 const app = require('./config/express')
 const mongoose = require('./config/mongoose')
 
 mongoose.connect()
 
-app.listen(port, () => logger.info(`server started on port ${port} (${env})`))
-
 module.exports = app
+module.exports.handler = serverless(app)
